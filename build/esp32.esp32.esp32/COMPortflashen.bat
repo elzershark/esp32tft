d9be885d2fdebbd1659e4d126c7be3aad8232583
Elzershark <elzershark@yahoo.de>
2023-08-18 14:40:05 +0200

    update

---
+++ build/esp32.esp32.esp32/COMPortflashen.bat
@@ -0,0 +1,42 @@
+@echo off
+goto END
+:END
+cls
+set COMPORT=COM3
+echo                 Zugangsdaten
+echo      ---------------------------------
+echo              COMPort= %COMPORT%
+echo      =================================
+echo.
+echo.
+echo.
+echo Diese Datei kann man mit einem Editor bearbeiten,
+echo zum setzen des COMPorts.
+echo Gibt man nichts ein, werden die Daten uebernommen
+echo.
+echo Ansonsten, jetzt hier die Daten eintragen
+echo.
+set /p COMPORT="Den COM Port komplett eintragen(z.B. COM3): "
+echo   [1] Ohne resetten (Bei updates)
+echo   [2] Mit resetten. Alles wird geloescht
+echo   [3] COM Ports anzeigen
+echo.
+set asw=0
+set /p asw="Bitte Auswahl eingeben: "
+if %asw%==1 goto NORMAL
+if %asw%==2 goto RESET
+if %asw%==3 goto COMP
+goto END
+:RESET
+echo "Am Display Boot gedrueckt lassen. Dann hier Enter: "
+pause
+start cmd /k esptool.exe --chip esp32 --port %COMPORT% --baud 921600  --before default_reset --after hard_reset write_flash -e -z --flash_mode dio --flash_freq 80m --flash_size 4MB 0x1000 esp32tft.ino.bootloader.bin 0x8000 esp32tft.ino.partitions.bin 0xe000 boot_app0.bin 0x10000 esp32tft.ino.bin
+goto END
+:NORMAL
+echo "Am Display Boot gedrueckt lassen. Dann hier Enter: "
+pause
+start cmd /k esptool.exe --chip esp32 --port %COMPORT% --baud 921600  --before default_reset --after hard_reset write_flash  -z --flash_mode dio --flash_freq 80m --flash_size 4MB 0x1000 esp32tft.ino.bootloader.bin 0x8000 esp32tft.ino.partitions.bin 0xe000 boot_app0.bin 0x10000 esp32tft.ino.bin
+goto END
+:COMP
+start cmd /k chgport
+goto END
---
+++ build/esp32.esp32.esp32/OTAflashen.bat
@@ -0,0 +1,29 @@
+@echo off
+goto NORMAL
+
+:NORMAL
+cls
+set IP=192.168.68.155
+set PSW=casio
+echo                 Zugangsdaten
+echo      ---------------------------------
+echo              IP= %IP%
+echo              Passwort= %PSW% 
+echo      =================================
+echo.
+echo.
+echo Diese Datei kann man mit einem Editor bearbeiten,
+echo zum setzen der IP Adresse und des Passwortes.
+echo Gibt man nichts ein, werden die Daten uebernommen
+echo.
+echo Ansonsten, jetzt hier die Daten eintragen
+echo.
+set /p IP="Die IP vom Display: "
+set /p PSW="Das Passwort von MQTT Benutzername: "
+echo "Am Display Boot gedrueckt lassen. Dann hier Enter: "
+pause
+goto END
+
+:END
+start cmd /k espota.exe -r -i %IP% -p 3232 --auth=%PSW% --timeout=10000 -f esp32tft.ino.bin
+goto NORMAL
